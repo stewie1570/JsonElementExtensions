35476a7b396b310e8c06ae863f156edd1357dbd2
Stewart Anderson <stewart.anderson3@extendhealth.com>
2023-03-29 14:09:24 -0600

    Added a program (input JSON -> paths output)

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using System.Text.Json;
+using dynamic_iteration;
+
+Func<string> ReadUnilEmpty = () =>
+{
+    var buffer = "";
+    string input;
+
+    while ((input = Console.ReadLine()) != "")
+    {
+        buffer += input;
+    }
+
+    return buffer;
+};
+
+Console.WriteLine("JSON: ");
+var json = ReadUnilEmpty();
+
+JsonDocument
+    .Parse(json)
+    .RootElement
+    .PathsAndValuesDictionary()
+    .Select(value => value.Key)
+    .ToList()
+    .ForEach(Console.WriteLine);
